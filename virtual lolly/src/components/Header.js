49cce4647f2cf9f7e7b80f02d8099b23d065ceac
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-05 16:37:25 +0500

    Storybook published

--- virtual lolly/src/components/Header.js
+++ virtual lolly/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
   return (
     <header>
-      <h1 className="mainheading">Virtual Lolly</h1>
+      <h1 className="mainheading">Virtual Lolly!</h1>
       <p className="subheading">Yes everyone gets a lollipop. SeemsGood.</p>
     </header>
   )
